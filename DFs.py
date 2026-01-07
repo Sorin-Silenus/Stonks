f6f97e95ffd01af25ff3e9c8ca03a82d40525936
unknown <sorinja50@gmail.com>
2026-01-07 10:16:54 -0500

    some random work

--- DFs.py
+++ DFs.py
@@ -1,9 +1,25 @@
+# File to test out some data frames with yfinance
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-rets = yf.download(tickers=["SPYI", "VYM"], interval='1d')["Close"]
 
-type(rets)
+rets = yf.download(["SPYI", "VYM"], interval='1d')["Close"]
+
+print(type(rets))
 print(rets.head())
+
+data = yf.Tickers("SPYI VYM SDY VXUS JEPI")
+divs = {"SPYI" : data.tickers['SPYI'].dividends, 
+        "VYM" : data.tickers['VYM'].dividends,
+        "SDY" : data.tickers['SDY'].dividends,
+        "VXUS" : data.tickers['VXUS'].dividends,
+        "JEPI" : data.tickers['JEPI'].dividends}  
+
+df = pd.DataFrame(divs).fillna(0).sort_index(ascending=False)
+df_monthly = df.resample('M').sum().sort_index(ascending=False)
+df_monthly.index = df_monthly.index.to_period("M")
+
+print(df_monthly.head())
+
